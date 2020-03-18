43926c1de6ed4adb37b7d15c5d3ff5d329b8b73d
Tonzzu <t.o.ruusunen@gmail.com>
2020-03-18 17:35:04 +0200

    Second commit

---
+++ src/List.js
@@ -0,0 +1,45 @@
+import React from "react";
+import uuid from "react-uuid";
+import Row from "./Row";
+import { Todo } from "./Todo";
+import Form from "./Form";
+
+export default class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { todos: new Array(new Todo("Test1", "Test description")) };
+  }
+
+  addToList = (title, description) => {
+    this.setState(prevState => ({
+      todos: [...prevState.todos, new Todo(title, description)]
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h3>Todos</h3>
+        <Form add={this.addToList} />
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.todos.map(item => (
+              <Row
+                key={uuid()}
+                title={item.title}
+                description={item.description}
+              />
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
